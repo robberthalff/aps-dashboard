cb2b4ff5467b032d9666af6aa115bb3a494ab459
Rob Halff <rob.halff@gmail.com>
2016-08-13 18:54:15 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import http from 'http'
+import ParseDashboard from 'parse-dashboard'
+import config from '../config'
+import objenv from 'objenv'
+import pkg from '../package.json'
+
+objenv(config, {prefix: 'APS', camelCase: true}, (key, value) => {
+  console.log('%s %s', key, value)
+})
+
+const dashboard = new ParseDashboard(
+  config,
+  Boolean(config.allowInsecureHTTP)
+)
+
+const app = express()
+
+app.use('/', dashboard)
+
+var httpServer = http.createServer(app)
+
+httpServer.listen(config.port, () => {
+  console.log('%s (%s) running on port %s.', pkg.name, pkg.version, config.port)
+})
