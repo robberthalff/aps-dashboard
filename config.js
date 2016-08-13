08b67edcf62a5ff233665b045a85ba2ccdcfaa0a
Rob Halff <rob.halff@gmail.com>
2016-08-13 18:58:21 +0200

    lint config

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	port: '4040',
-	allowInsecureHTTP: '1',
-	apps: [
-		{
-			serverURL: 'http://localhost:1337',
-			appId: 'myAppId',
-			masterKey: 'myMasterKey',
-			appName: 'MyApp'
-		}
-	]
+  port: '4040',
+  allowInsecureHTTP: '1',
+  apps: [
+    {
+      serverURL: 'http://localhost:1337',
+      appId: 'myAppId',
+      masterKey: 'myMasterKey',
+      appName: 'MyApp'
+    }
+  ]
 }
